a365a3c1a55008b83dd2bf5a86b6ecc1c56125c6
Hussain Al Kazemi <mhk.alk@gmail.com>
2019-10-09 20:22:35 +0300

    Completed task 10 Hussain & Nada

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
@@ -6,6 +8,19 @@ class Restaurant(models.Model):
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+   		 return reverse("restaurant-detail", kwargs={"restaurant_id": self.id})
 
     def __str__(self):
     	return self.name
+
+class Item(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.TextField()
+	price = models.FloatField()
+	restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+	def get_absolute_url(self):
+   		 return reverse("restaurant-detail", kwargs={"restaurant_id": self.id})
